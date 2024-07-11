f8f631fff5d2cdadf5913b46f899c559bb2b0100
Akash M Nandan <akash003nandan@gmail.com>
2024-07-11 11:59:09 +0530

    Customer model created

--- BroKart/Customers/models.py
+++ BroKart/Customers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# model for customer
+
+class Customers(models.Model):
+    LIVE = 1     
+    DELETE = 0           
+    delete_choices = ((LIVE, 'Live'), (DELETE, 'Delete'))    
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer_profile')
+    address = models.TextField()
+    delete_status = models.IntegerField(choices=delete_choices, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.name
