56ae5f0ad358882ab77c9a95234aece9888b50fe
Akash M Nandan <akash003nandan@gmail.com>
2024-07-11 17:21:14 +0530

    Cart  model created

--- BroKart/Orders/models.py
+++ BroKart/Orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Customers.models import Customers
 
-# Create your models here.
+# Cart model for storing cart items
+
+class Cart(models.Model):
+    LIVE = 1
+    DELETE = 0
+    delete_choices = ((LIVE, 'Live'), (DELETE, 'Delete'))
+    
+    owner = models.ForeignKey(Customers, on_delete=models.CASCADE, related_name='orders') # owner of the cart is a customer
+    delete_status = models.IntegerField(choices=delete_choices, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+ 
