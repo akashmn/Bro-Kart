06ee263c771de69a01f92165b336d1390ceec1c8
Akash M Nandan <akash003nandan@gmail.com>
2024-07-11 21:31:21 +0530

    ordered items  model created

--- BroKart/Orders/models.py
+++ BroKart/Orders/models.py
@@ -1,15 +1,34 @@
 from django.db import models
 from Customers.models import Customers
+from Products.models import Product
 
 # Cart model for storing cart items
 
-class Cart(models.Model):
+class Order(models.Model):
     LIVE = 1
     DELETE = 0
+
+    CART_STAGE = 0 # cart stage is when the order is created but not confirmed by the customer
+    ORDER_CONFIRMED = 1  # order confirmed by the customer and ready to be processed
+    ORDER_PROCESSED = 2 # order is processed and ready to be delivered to the customer
+    ORDER_DELIVERED = 3 # order is delivered to the customer successfully
+    ORDER_REJECTED = 4 # order is rejected by the customer or the admin
+
+    order_choices = ((ORDER_CONFIRMED, 'Order Confirmed'), 
+                     (ORDER_PROCESSED, 'Order Processed'), 
+                     (ORDER_DELIVERED, 'Order Delivered'), 
+                     (ORDER_REJECTED, 'Order Rejected')) # choices for the order status 
+
+    order_status = models.IntegerField(choices=order_choices, default=CART_STAGE)
+
     delete_choices = ((LIVE, 'Live'), (DELETE, 'Delete'))
-    
     owner = models.ForeignKey(Customers, on_delete=models.CASCADE, related_name='orders') # owner of the cart is a customer
     delete_status = models.IntegerField(choices=delete_choices, default=LIVE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
- 
+
+
+class OrderedItem(models.model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, related_name='added_carts')
+    quantity = models.IntegerField(default=1)
+    owner = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='added_items')
