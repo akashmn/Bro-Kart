b0c82641da719c46bae26e870859decb90724d78
Akash M Nandan <akash003nandan@gmail.com>
2024-07-13 14:36:04 +0530

    product page created

--- BroKart/Products/urls.py
+++ BroKart/Products/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('product_list/', views.list_product, name='list_product'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
