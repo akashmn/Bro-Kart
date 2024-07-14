d8cbdd204e439e2dee458d2403b7bf46a90d1bbc
Akash M Nandan <akash003nandan@gmail.com>
2024-07-14 13:00:18 +0530

    product page urls and views sett

--- BroKart/BroKart/urls.py
+++ BroKart/BroKart/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('Products.urls')),
+    path('customer/', include('Customers.urls')),
 
 ]
 
