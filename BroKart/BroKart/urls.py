5149e0fbce15acfdab67113ebe9ec9127aed818b
Akash M Nandan <akash003nandan@gmail.com>
2024-07-15 00:07:40 +0530

    cart layout set

--- BroKart/BroKart/urls.py
+++ BroKart/BroKart/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('Products.urls')),
     path('customer/', include('Customers.urls')),
-
+    path('orders/', include('Orders.urls')),
 ]
 
 
